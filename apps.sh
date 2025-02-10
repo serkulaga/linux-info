52de95972786d2f895bf5b9c3bfb7ba422a0652e
serkulaga <serkulaga@gmail.com>
2025-02-10 23:00:52 +0400

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -3,28 +3,22 @@
 check_command() {
     local cmd=$1
     if command -v $cmd &> /dev/null; then
-        case "$cmd" in
-            nginx | node | wp)
-                local version=$($cmd -v 2>&1 | head -n 1)
-                ;;
-            redis-server)
-                local version=$($cmd --version 2>&1 | grep -oP 'v=\K\S+')
-                ;;
-            kubectl)
-                local version=$($cmd version --client --short 2>&1)
-                ;;
-            *)
-                local version=$($cmd --version 2>&1 | head -n 1)
-                ;;
-        esac
+        if [[ "$cmd" == "nginx" || "$cmd" == "node" || "$cmd" == "wp" ]]; then
+            local version=$($cmd -v 2>&1 | head -n 1)
+        elif [[ "$cmd" == "redis-server" ]]; then
+            local version=$($cmd --version 2>&1 | grep -oP 'v=\K\S+')
+        else
+            local version=$($cmd --version 2>&1 | head -n 1)
+        fi
         local path=$(command -v $cmd)
         echo -e "$cmd\t$version\t$path"
     fi
 }
+
 # List of commands to check
 commands=(
     "mysql" "docker" "nginx" "apache2" "php" "python" "git" "node" "ruby"
-    "java" "go" "rust" "kubectl" "terraform" "django" "pip" "redis-server"
+    "java" "go" "rust" "terraform" "django" "pip" "redis-server"
     "wp" "postgres" "mongodb" "sqlite3" "haproxy" "memcached"
     "openvpn" "samba"
 )
