39b85cc28aaf14c861b404fdab9e2e42e205de4a
serkulaga <serkulaga@gmail.com>
2025-02-10 12:47:33 +0400

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -16,10 +16,10 @@ echo "Scanning network: $IP_RANGE"
 
 # Define output file
 OUTPUT_FILE="nmap_scan_results.csv"
-echo "IP Address, MAC Address, Vendor, Hostname, OS, Uptime, Open Ports, Protocols, Services" > "$OUTPUT_FILE"
+echo "IP Address, MAC Address, Vendor, Hostname, OS, Open Ports, Protocols, Services" > "$OUTPUT_FILE"
 
 # Perform an advanced scan
-sudo nmap -O -sV -p- --script banner,uptime,mac-address,vuln "$IP_RANGE" -oG - | awk '
+sudo nmap -O -sV -p- --script=default "$IP_RANGE" -oG - | awk '
 /Up$/{ip=$2}
 /MAC Address:/{mac=$3; vendor=substr($0, index($0,$4))}
 /Ports:/{split($0, p, "Ports: "); split(p[2], ports, ", "); open_ports=""; protocols=""; services="";
@@ -29,7 +29,7 @@ sudo nmap -O -sV -p- --script banner,uptime,mac-address,vuln "$IP_RANGE" -oG - |
         protocols = protocols details[2] " ";
         services = services details[5] " ";
     }
-    print ip "," mac "," vendor "," $5 "," $4 "," $6 "," open_ports "," protocols "," services
+    print ip "," (mac ? mac : "N/A") "," (vendor ? vendor : "Unknown") "," $5 "," $4 "," open_ports "," protocols "," services
 }' >> "$OUTPUT_FILE"
 
 echo "Scan complete. Results saved to $OUTPUT_FILE."
