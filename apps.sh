94c5f3f52fb33d7c2d5fe29d2f56d80eb9a0e497
serkulaga <serkulaga@gmail.com>
2025-02-10 22:56:45 +0400

    Create apps.sh

---
+++ apps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+check_command() {
+    local cmd=$1
+    if command -v $cmd &> /dev/null; then
+        if [[ "$cmd" == "nginx" || "$cmd" == "node" || "$cmd" == "wp" ]]; then
+            local version=$($cmd -v 2>&1 | head -n 1)
+        elif [[ "$cmd" == "redis-server" ]]; then
+            local version=$($cmd --version 2>&1 | grep -oP 'v=\K\S+')
+        else
+            local version=$($cmd --version 2>&1 | head -n 1)
+        fi
+        local path=$(command -v $cmd)
+        echo -e "$cmd\t$version\t$path"
+    fi
+}
+
+# List of commands to check
+commands=(
+    "mysql" "docker" "nginx" "apache2" "php" "python" "git" "node" "ruby"
+    "java" "go" "rust" "kubectl" "terraform" "django" "pip" "redis-server"
+    "wp" "postgres" "mongodb" "sqlite3" "haproxy" "memcached"
+    "openvpn" "samba"
+)
+
+# Print table header
+echo -e "Application\tVersion\tPath"
+
+# Loop through commands and check each one
+for cmd in "${commands[@]}"; do
+    check_command $cmd
+done
