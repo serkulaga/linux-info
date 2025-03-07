0d19ede8dd36bca4ff6a0c38621d7d6e24633c42
serkulaga <serkulaga@gmail.com>
2025-03-07 14:11:01 +0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Print CSV header
-printf "CONTAINER_NAME\tIMAGE\tPORTS\tCPU\tMEMORY_USED\tMEMORY_ALLOCATED\tMEMORY_PERCENTAGE"
+printf "CONTAINER_NAME\tIMAGE\tPORTS\tCPU\tMEMORY_USED\tMEMORY_ALLOCATED\tMEMORY_PERCENTAGE\n"
 
 # Loop through running containers
 docker ps --format "{{.Names}},{{.Image}},{{.Ports}}" | while IFS=, read -r name image ports; do
@@ -16,5 +16,5 @@ docker ps --format "{{.Names}},{{.Image}},{{.Ports}}" | while IFS=, read -r name
     memory_percentage=$(echo "$stats" | cut -d',' -f3 | sed 's/%//')
 
     # Print the result in CSV format
-    printf "$name\t$image\t$ports\t$cpu_usage\t$memory_usage\t$memory_allocated\t$memory_percentage"
+    printf "$name\t$image\t$ports\t$cpu_usage\t$memory_usage\t$memory_allocated\t$memory_percentage\n"
 done
