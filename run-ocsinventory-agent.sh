b49d2d6aec4cdcd9fc99f865840ef2d621fd5d97
serkulaga <serkulaga@gmail.com>
2025-04-08 20:53:18 +0400

    Update run-ocsinventory-agent.sh

--- run-ocsinventory-agent.sh
+++ run-ocsinventory-agent.sh
@@ -14,7 +14,7 @@ sudo apt-get update
 
 # Pre-seed the debconf values including the custom "tag" setting
 sudo debconf-set-selections <<EOF
-ocsinventory-agent  ocsinventory-agent/method   string  http
+ocsinventory-agent  ocsinventory-agent/method   select  http
 ocsinventory-agent  ocsinventory-agent/server   string  ${OCS_SERVER}
 ocsinventory-agent  ocsinventory-agent/tag      string  ${OCS_TAG}
 EOF
